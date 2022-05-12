b6b1a23d0d91f5f8a3d0eef5aaf61f8df4aafde2
Waqas Tahir <wakaztahir@gmail.com>
2022-05-12 15:14:54 +0100

    span fix

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,45 +2,46 @@ import {StaggeredDisplay, StaggeredGrid, StaggeredGridItem, StaggeredItemSpan} f
 
 function App() {
 
-  type Item = {
-    id: number,
-    width: number,
-    height: number,
-    span: StaggeredItemSpan
-  }
+    type Item = {
+        id: number,
+        span: StaggeredItemSpan,
+        width: string,
+        height: number,
+    }
 
-  const items: Array<Item> = []
+    const items: Array<Item> = []
 
-  for (let i = 0; i < 100; i++) {
-    items.push({
-      id: i,
-      width: 300,
-      height: (Math.random() * 300) + 300,
-      span: i % 7 === 0 ? StaggeredItemSpan.Full : StaggeredItemSpan.Single
-    });
-  }
+    for (let i = 0; i < 100; i++) {
+        let span = i % 10 === 0 ? StaggeredItemSpan.Full : StaggeredItemSpan.Single
+        items.push({
+            id: i,
+            span,
+            width: span === StaggeredItemSpan.Full ? "100%" : "300px",
+            height: (Math.random() * 300) + 300,
+        });
+    }
 
-  return (
-      <StaggeredGrid
-          display={StaggeredDisplay.Grid}
-          items={items}
-          render={(item: Item, index: number) => (
-              <StaggeredGridItem index={index} key={index} style={{transition: "transform 0.3s ease"}}>
-                <div style={{
-                  width: "300px",
-                  height: item.height + "px",
-                  background: "skyblue",
-                  textAlign: "center",
-                  lineHeight: item.height + "px",
-                  margin: "8px"
-                }}>
-                  Item {index}
-                </div>
-              </StaggeredGridItem>
-          )
-          }
-      />
-  )
+    return (
+        <StaggeredGrid
+            display={StaggeredDisplay.Grid}
+            items={items}
+            render={(item: Item, index: number) => (
+                <StaggeredGridItem index={index} key={index} spans={item.span} style={{transition: "transform 0.3s ease"}}>
+                    <div style={{
+                        width: item.width,
+                        height: item.height + "px",
+                        background: "skyblue",
+                        textAlign: "center",
+                        lineHeight: item.height + "px",
+                        margin: "8px"
+                    }}>
+                        Item {index}
+                    </div>
+                </StaggeredGridItem>
+            )
+            }
+        />
+    )
 }
 
 export default App;
