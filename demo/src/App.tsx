d2ac352ec9d74989790315269f92b2928cfcd8ba
Waqas Tahir <wakaztahir@gmail.com>
2022-05-16 11:39:04 +0100

    demo improved

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,4 +1,11 @@
-import {StaggeredDisplay, StaggeredGrid, StaggeredGridItem, StaggeredItemSpan} from "react-staggered-grid";
+import React, {useState} from "react"
+import {
+    StaggeredAlignment,
+    StaggeredDisplay,
+    StaggeredGrid,
+    StaggeredGridItem,
+    StaggeredItemSpan
+} from "react-staggered-grid";
 
 function App() {
 
@@ -21,26 +28,72 @@ function App() {
         });
     }
 
+    const [alignment, setAlignment] = useState(StaggeredAlignment.Center)
+    const [display,setDisplay] = useState(StaggeredDisplay.Grid)
+
     return (
-        <StaggeredGrid
-            display={StaggeredDisplay.Grid}
-            items={items}
-            render={(item: Item, index: number) => (
-                <StaggeredGridItem index={index} key={index} spans={item.span} style={{transition: "transform 0.3s ease"}}>
-                    <div style={{
-                        width: item.width,
-                        height: item.height + "px",
-                        background: "skyblue",
-                        textAlign: "center",
-                        lineHeight: item.height + "px",
-                        margin: "8px"
-                    }}>
-                        Item {index}
-                    </div>
-                </StaggeredGridItem>
-            )
-            }
-        />
+        <React.Fragment>
+            <StaggeredOptions
+                alignment={alignment}
+                setAlignment={setAlignment}
+                display={display}
+                setDisplay={setDisplay}
+            />
+            <StaggeredGrid
+                display={display}
+                alignment={alignment}
+                items={items}
+                render={(item: Item, index: number) => (
+                    <StaggeredGridItem index={index} key={index} spans={item.span}
+                                       style={{transition: "transform 0.3s ease"}}>
+                        <div style={{
+                            width: item.width,
+                            height: item.height + "px",
+                            background: "skyblue",
+                            textAlign: "center",
+                            lineHeight: item.height + "px",
+                            margin: "8px"
+                        }}>
+                            Item {index}
+                        </div>
+                    </StaggeredGridItem>
+                )
+                }
+            />
+        </React.Fragment>
+    )
+}
+
+interface Options {
+    alignment: StaggeredAlignment,
+    setAlignment: (alignment: StaggeredAlignment) => void;
+    display: StaggeredDisplay,
+    setDisplay: (display: StaggeredDisplay) => void;
+}
+
+function StaggeredOptions(props: Options) {
+    return (
+        <div style={{width: "100%", display: "flex", justifyContent: "center", marginTop: "1em"}}>
+            <select
+                value={props.alignment}
+                onChange={(e) => {
+                    props.setAlignment(parseInt(e.currentTarget.value))
+                }}
+            >
+                <option value={StaggeredAlignment.Start}>Alignment : Start</option>
+                <option value={StaggeredAlignment.Center}>Alignment : Center</option>
+                <option value={StaggeredAlignment.End}>Alignment : End</option>
+            </select>
+            <select
+                onChange={(e) => {
+                    props.setDisplay(parseInt(e.currentTarget.value))
+                }}
+                value={props.display}
+            >
+                <option value={StaggeredDisplay.Linear}>Display : Linear</option>
+                <option value={StaggeredDisplay.Grid}>Display : Grid</option>
+            </select>
+        </div>
     )
 }
 
